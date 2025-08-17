77a519e4d8075f79e3a16df617b1f271b793e16c
kinghoys <yhoys2004@gmail.com>
2025-08-17 21:24:51 +0530

    fix: deployment configuration for vercel

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
     unoptimized: true,
   },
-  output: 'standalone',
 }
 
 module.exports = nextConfig
